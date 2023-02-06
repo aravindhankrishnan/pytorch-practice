c13ed8aade3020c233e9499afa57aa06192bbade
aravindhan <aravindhan.krishnan@gmail.com>
2023-02-05 17:17:10 -0800

    MLP working version

--- mlp.py
+++ mlp.py
@@ -8,16 +8,17 @@ class CircleModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(in_features=2, out_features=10),
+            nn.Linear(in_features=2, out_features=5),
             nn.ReLU(),
-            nn.Linear(in_features=10, out_features=10),
+            nn.Linear(in_features=5, out_features=5),
             nn.ReLU(),
-            nn.Linear(in_features=10, out_features=10),
+            nn.Linear(in_features=5, out_features=5),
             nn.ReLU(),
-            nn.Linear(in_features=10, out_features=1),
+            nn.Linear(in_features=5, out_features=1),
             nn.ReLU()
         )
-    def forward(self, x) -> torch.Tensor:
+
+    def forward(self, x):
         return self.mlp(x)
 
 def setup_training_data():
@@ -29,51 +30,58 @@ def setup_training_data():
 
 def estimate_accuracy(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
-    acc = (correct / len(y_pred)) * 100 
-    return acc
+    accuracy = (correct / len(y_pred)) * 100 
+    return accuracy
 
 def main():
-    torch.manual_seed(100)
     x, y = setup_training_data()
 
     model = CircleModel()
-    epochs = 5000
-    check_point = epochs / 10
+    print(model)
+    epochs = 100000
+    check_point = 1000
 
-    loss_function = nn.BCEWithLogitsLoss()
+    loss_function = nn.BCELoss()
     optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+    print(optimizer)
+
+    torch.manual_seed(100)
 
     for epoch in range(epochs):
         # Put model in training mode
         model.train()
-
+    
         # Compute loss
-        y_pred = model(x)
-        loss = loss_function(y_pred.squeeze(), y)
-
-        y_prob = torch.sigmoid(y_pred)
-        y_pred_labels = torch.round(y_pred).squeeze()
-
+        y_pred = model(x).squeeze()
+        y_logits = torch.sigmoid(y_pred)
+        loss = loss_function(y_logits, y)
+        
+        y_pred_labels = torch.round(y_logits).squeeze()
         accuracy = estimate_accuracy(y, y_pred_labels)
-
+    
         # Invoke the optimizer
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+    
         # Put the model in evaluation mode
+        if epoch % check_point:
+            continue
+
         model.eval()
         with torch.inference_mode():
-            y_pred = model(x)
-            training_loss = loss_function(y_pred.squeeze(), y)
+            y_pred = model(x).squeeze()
+            y_logits = torch.sigmoid(y_pred)
+            loss = loss_function(y_logits, y)
+            y_pred_labels = torch.round(y_logits).squeeze()        
 
-            y_prob = torch.sigmoid(y_pred)
-            y_pred_labels = torch.round(y_prob).squeeze()        
             accuracy = estimate_accuracy(y, y_pred_labels)
+            loss = loss.detach().numpy()
+            print(f"Epoch: {epoch} | Accuracy: {accuracy} | Train Loss: {loss}")
 
-            if epoch % check_point == 0:
-                training_loss_value = training_loss.detach().numpy()
-                print(f"Epoch: {epoch} | Accuracy: {accuracy} | Train Loss: {training_loss_value}")
+            if accuracy > 99.0:
+                break
+
 
 if __name__ == '__main__':
     main()
