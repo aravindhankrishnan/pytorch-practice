31806c6a47b134b7e1282d7b6f080d11661bec8a
aravindhan <aravindhan.krishnan@gmail.com>
2023-02-05 21:07:50 -0800

    Fixing training issues - Removing relu activation in the output layer

--- multi_class_mlp.py
+++ multi_class_mlp.py
@@ -4,25 +4,34 @@ import numpy as np
 from sklearn import datasets
 from matplotlib import pyplot as plt
 
-class CircleModel(nn.Module):
+RANDOM_SEED = 42
+
+class BlobModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(in_features=2, out_features=8),
-            nn.ReLU(),
-            nn.Linear(in_features=8, out_features=8),
-            nn.ReLU(),
-            nn.Linear(in_features=8, out_features=1)
-        )
+                nn.Linear(in_features=2, out_features=8),
+                nn.ReLU(),
+                nn.Linear(in_features=8, out_features=8),
+                nn.ReLU(),
+                nn.Linear(in_features=8, out_features=4),
+                #nn.ReLU()
+                )
 
     def forward(self, x):
         return self.mlp(x)
 
 def setup_training_data():
     nsamples = 1000
-    x_np, y_np = datasets.make_circles(nsamples, noise=0.03, random_state=100) 
+    x_np, y_np = datasets.make_blobs(n_samples=1000,
+        n_features=2,
+        centers=4,
+        cluster_std=1.5,
+        random_state=RANDOM_SEED
+    )
+
     x = torch.from_numpy(x_np).type(torch.float)
-    y = torch.from_numpy(y_np).type(torch.float)
+    y = torch.from_numpy(y_np).type(torch.LongTensor)
     return x, y
 
 def estimate_accuracy(y_true, y_pred):
@@ -33,16 +42,14 @@ def estimate_accuracy(y_true, y_pred):
 def main():
     x, y = setup_training_data()
 
-    model = CircleModel()
-    print(model)
+    model = BlobModel()
     epochs = 100000
     check_point = 1000
 
-    loss_function = nn.BCELoss()
+    loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
-    print(optimizer)
 
-    torch.manual_seed(100)
+    torch.manual_seed(RANDOM_SEED)
 
     for epoch in range(epochs):
         # Put model in training mode
@@ -50,11 +57,10 @@ def main():
     
         # Compute loss
         y_pred = model(x).squeeze()
-        y_logits = torch.sigmoid(y_pred)
-        loss = loss_function(y_logits, y)
+        y_softmax = torch.softmax(y_pred, dim=1)
+        loss = loss_function(y_softmax, y)
         
-        y_pred_labels = torch.round(y_logits).squeeze()
-        accuracy = estimate_accuracy(y, y_pred_labels)
+        y_pred_labels = y_softmax.argmax(dim=1).squeeze()
     
         # Invoke the optimizer
         optimizer.zero_grad()
@@ -67,10 +73,12 @@ def main():
 
         model.eval()
         with torch.inference_mode():
+            # Compute loss
             y_pred = model(x).squeeze()
-            y_logits = torch.sigmoid(y_pred)
-            loss = loss_function(y_logits, y)
-            y_pred_labels = torch.round(y_logits).squeeze()        
+            y_softmax = torch.softmax(y_pred, dim=1)
+            loss = loss_function(y_softmax, y)
+            
+            y_pred_labels = y_softmax.argmax(dim=1).squeeze()
 
             accuracy = estimate_accuracy(y, y_pred_labels)
             loss = loss.detach().numpy()
