69017799bfd9d70e61c786ea71fb371164c0acb5
aravindhan <aravindhan.krishnan@gmail.com>
2023-02-09 20:01:04 -0800

    Fashion mnist works. Supporting linear and relu models

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -1,3 +1,4 @@
+import argparse
 import functools
 import os
 import operator
@@ -10,7 +11,20 @@ from torchvision import (
 
 HIDDEN_UNITS = 10
 
-class FashionMNISTClassifier(nn.Module):
+class FashionMNISTClassifierLinear(nn.Module):
+    def __init__(self, input_dims, num_classes):
+        super().__init__()
+        n = functools.reduce(operator.mul, input_dims)
+        self.mlp = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(in_features=n, out_features=HIDDEN_UNITS),
+                nn.Linear(in_features=HIDDEN_UNITS, out_features=HIDDEN_UNITS),
+                )
+
+    def forward(self, x):
+        return self.mlp(x)
+
+class FashionMNISTClassifierReLU(nn.Module):
     def __init__(self, input_dims, num_classes):
         super().__init__()
         n = functools.reduce(operator.mul, input_dims)
@@ -20,7 +34,6 @@ class FashionMNISTClassifier(nn.Module):
                 nn.ReLU(),
                 nn.Linear(in_features=HIDDEN_UNITS, out_features=HIDDEN_UNITS),
                 nn.ReLU(),
-                nn.Linear(in_features=HIDDEN_UNITS, out_features=num_classes),
                 )
 
     def forward(self, x):
@@ -34,24 +47,34 @@ def evaluate_model(train_dataloader, model, loss_function, device):
 
     train_loss = 0
     true_positives = 0.
+    total = 0
     with torch.inference_mode():
         for (x, y) in train_dataloader:
             x.to(device)
             y.to(device)
+            
             y_pred = model(x).squeeze()
             y_softmax = torch.softmax(y_pred, dim=1)
+
             loss = loss_function(y_softmax, y)
+            train_loss += loss.detach().numpy()
+
             y_pred_labels = y_softmax.argmax(dim=1).squeeze()
 
-            train_loss += loss.detach().numpy()
-            true_positives = count_true_positives(y, y_pred_labels)
+            true_positives += count_true_positives(y, y_pred_labels)
+            total += len(y_pred_labels)
 
-    accuracy = 100. * true_positives / len(train_dataloader)
+    accuracy = 100. * true_positives / total
     train_loss /= len(train_dataloader)
 
     return train_loss, accuracy
 
 def main():
+    parser = argparse.ArgumentParser(description='Fashion MNIST model')                       
+    parser.add_argument('--model', type=str, help='Model type', required=True, choices=['linear', 'relu'])    
+
+    args = parser.parse_args()
+
     train_data = datasets.FashionMNIST(
             root=os.path.expanduser('~/datasets/fashion_mnist'),
             train=True,
@@ -63,6 +86,7 @@ def main():
     print(len(train_data))
     image, _ = train_data[0]
     image_shape, num_classes = image.shape, len(train_data.classes)
+    print('Num classes = ', num_classes)
 
     batch_size = 32
     train_dataloader = DataLoader(train_data, batch_size, shuffle=True)
@@ -70,15 +94,24 @@ def main():
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print('Device = ', device)
-    model = FashionMNISTClassifier(image_shape, num_classes).to(device)
+    
+    if args.model == 'linear':
+        model = FashionMNISTClassifierLinear(image_shape, num_classes).to(device)
+    elif args.model == 'relu':
+        model = FashionMNISTClassifierReLU(image_shape, num_classes).to(device)
+
+    print(model)
 
     loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
-    epochs = 10
+    torch.manual_seed(100)
+
+    epochs = 100
 
     for epoch in range(epochs):
         model.train()
+        train_loss = 0
         for batch_id, (x, y) in enumerate(train_dataloader):
             x.to(device)
             y.to(device)
@@ -93,7 +126,6 @@ def main():
 
         loss, accuracy = evaluate_model(train_dataloader, model, loss_function, device)
         print(f'Epoch = {epoch} | Loss {loss} | Accuracy {accuracy}')
-
 
 if __name__ == '__main__':
     main()
